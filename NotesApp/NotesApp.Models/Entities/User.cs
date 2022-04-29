ae7aaf9896fa4001caa836af66cc912f9a91ffae
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-04-29 12:27:32 +0200

    Add base db context

---
+++ NotesApp/NotesApp.Models/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿namespace NotesApp.Models.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Login { get; set; }
+        public virtual ICollection<Note> Notes { get; set; }
+    }
+}
