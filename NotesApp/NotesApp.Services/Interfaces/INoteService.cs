976555a13e401c78e45d5564b65ab19176d0af84
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-05-03 16:28:40 +0200

    Update notes service

---
+++ NotesApp/NotesApp.Services/Interfaces/INoteService.cs
@@ -0,0 +1,15 @@
+﻿using NotesApp.Domain.Entities;
+using NotesApp.Services.Dto;
+
+namespace NotesApp.Services.Interfaces
+{
+    public interface INoteService
+    {
+        Task<NoteDto?> GetNoteById(int id);
+        Task<IEnumerable<NoteDto>> GetAllNotes();
+        Task<IEnumerable<NoteDto>> GetNotesByName(string name);
+        Task<IEnumerable<NoteDto>> GetNotesByContent(string content);
+        Task<IEnumerable<NoteDto>> GetNotesByTags(IEnumerable<string> tags);
+        Task<int> AddNote(CreateNoteDto noteDto);
+    }
+}
