f41fcf438f0ef0ad16e20ecf4f113432838e5a8e
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-05-14 18:08:13 +0200

    Add outlines

--- NotesApp/notes-ui/src/components/inputForm.js
+++ NotesApp/notes-ui/src/components/inputForm.js
@@ -4,10 +4,26 @@ import './inputForm.css'
 const InputForm = (props) => {
   const {label, name, onChange, errorMessage, isValid, isFocused, onFocus, ...inputProps} = props;
 
+  const getClassName = () => {
+    if(!isValid && isFocused)
+      return 'input-invalid';
+    else if(isValid && isFocused)
+      return 'input-valid';
+    else 
+      return '';
+  }
+
   return (
     <div className='input-form'>
         <label htmlFor={name}>{label}</label>
-        <input name={name} {...inputProps} onChange={onChange} onFocus={onFocus} required/>
+        <input 
+          name={name} 
+          {...inputProps} 
+          onChange={onChange} 
+          onFocus={onFocus} 
+          required
+          className={getClassName()}
+        />
         <span className={!isValid && isFocused ? 'error-msg' : 'hide'}>{errorMessage}</span>
     </div>
   )
