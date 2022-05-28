28f4f4655b706338f78cdfeb466cda3f3d0bec38
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-05-28 15:49:24 +0200

    Add password regex

--- NotesApp/NotesApp.Services/Dto/Validators/CreateUserValidator.cs
+++ NotesApp/NotesApp.Services/Dto/Validators/CreateUserValidator.cs
@@ -21,13 +21,13 @@ namespace NotesApp.Services.Dto.Validators
                 .NotEmpty()
                 .MinimumLength(1)
                 .MaximumLength(20)
-                .Matches(@"[A-Za-z]*");
+                .Matches("[A-Za-z]");
 
             RuleFor(x => x.Surname)
                 .NotEmpty()
                 .MinimumLength(1)
                 .MaximumLength(20)
-                .Matches(@"[A-Za-z]*");
+                .Matches("[A-Za-z]");
 
             RuleFor(x => x.Email).EmailMustBeUnique(usersRepository);
 
@@ -36,7 +36,8 @@ namespace NotesApp.Services.Dto.Validators
             RuleFor(x => x.Password)
                 .NotEmpty()
                 .MinimumLength(6)
-                .MaximumLength(20);
+                .MaximumLength(20)
+                .Matches(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{6,20}$");
 
             RuleFor(x => x.ConfirmPassword).Equal(e => e.Password).WithMessage("Passwords should match");
         }
