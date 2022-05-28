ac327d65e9d3c00da42eab2face9dad2f6773240
Wiktoria Keller <65568180+wiktoriakeller@users.noreply.github.com>
2022-05-29 01:08:41 +0200

    Add integration tests project

---
+++ NotesApp/NotesApp.IntegrationTests/Controllers/NoteControllerTests.cs
@@ -0,0 +1,90 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Threading.Tasks;
+using FluentAssertions;
+using System.Net.Http;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NotesApp.DataAccess;
+using Microsoft.Extensions.DependencyInjection;
+using NotesApp.Services.Dto;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text;
+using Microsoft.AspNetCore.Authorization.Policy;
+using NotesApp.IntegrationTests.Extensions;
+
+namespace NotesApp.IntegrationTests.Controllers
+{
+    public class NoteControllerTests
+    {
+        private readonly HttpClient _client;
+
+        public NoteControllerTests()
+        {
+            var factory = new WebApplicationFactory<Program>();
+            _client = factory
+                .WithWebHostBuilder(builder => {
+                    builder.ConfigureServices(services =>
+                    {
+                        var dbContextOptions = services.SingleOrDefault(service => service.ServiceType == typeof(DbContextOptions<NotesDbContext>));
+                        services.Remove(dbContextOptions);
+
+                        services.AddSingleton<IPolicyEvaluator, FakePolicyEvaluator>();
+
+                        services.AddMvc(option => option.Filters.Add(new FakeUserFilter()));
+
+                        services.AddDbContext<NotesDbContext>(options => options.UseInMemoryDatabase("NotesInMemoryDb"));
+                    });
+                 })
+                .CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAllNotes_WithoutQueryParameters_ReturnsSuccess()
+        {
+            var response = await _client.GetAsync("/notes-api/notes");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task CreateNote_WithValidModel_ReturnsCreatedStatus()
+        {
+            var model = new CreateNoteDto
+            {
+                NoteName = "TestName",
+                Content = "Test content",
+                ImageLink = "",
+                Tags = new List<CreateTagDto>
+                {
+                    new CreateTagDto { TagName = "Test tag1" },
+                    new CreateTagDto { TagName = "Test tag2" }
+                }
+            };
+
+            var content = model.ToHttpContent();
+            var response = await _client.PostAsync("/notes-api/notes", content);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+            response.Headers.Location.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task CreateNote_WithInvalidModel_ReturnsBadRequest()
+        {
+            var model = new CreateNoteDto
+            {
+                NoteName = "",
+                Content = "",
+                ImageLink = "",
+                Tags = new List<CreateTagDto>() { }
+            };
+
+            var content = model.ToHttpContent();
+            var response = await _client.PostAsync("/notes-api/notes", content);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+    }
+}
---
+++ NotesApp/NotesApp.IntegrationTests/FakePolicyEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+using Microsoft.AspNetCore.Http;
+
+namespace NotesApp.IntegrationTests
+{
+    public class FakePolicyEvaluator : IPolicyEvaluator
+    {
+        public Task<AuthenticateResult> AuthenticateAsync(AuthorizationPolicy policy, HttpContext context)
+        {
+            var claimsPrincipal = new ClaimsPrincipal();
+            var ticket = new AuthenticationTicket(claimsPrincipal, "Test");
+            var result = AuthenticateResult.Success(ticket);
+            return Task.FromResult(result);
+        }
+
+        public Task<PolicyAuthorizationResult> AuthorizeAsync(AuthorizationPolicy policy, AuthenticateResult authenticationResult, HttpContext context, object? resource)
+        {
+            var result = PolicyAuthorizationResult.Success();
+            return Task.FromResult(result);
+        }
+    }
+}
